553316e3b06b2c023d1bb16580be6aaae3d7d941
Shubham Kumar <writetoshubham@gmail.com>
2019-11-28 15:30:55 +0530

    added more filter method

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -8,12 +8,30 @@ import domain.Dish;
 public class MainMenu {
 
 	public static void main(String[] args) {
+
 		List<String> threeHighCaloricDishNames = getMenu().stream().filter((Dish dish) -> dish.getCalories() > 300)
 				.map(Dish::getName).limit(3).collect(Collectors.toList());
 		System.out.println(threeHighCaloricDishNames);
+
 		List<String> sortedBasedOnCalories = getMenu().stream().sorted(Comparator.comparing(Dish::getCalories))
 				.map(Dish::getName).collect(Collectors.toList());
 		System.out.println(sortedBasedOnCalories);
+
+		List<Dish> vegetarianDishes = getMenu().stream().filter(Dish::isVegetarian).collect(Collectors.toList());
+		System.out.println(vegetarianDishes);
+
+		List<Integer> numbers = Arrays.asList(1, 2, 1, 3, 3, 2, 4);
+		numbers.stream().filter(i -> i % 2 == 0).distinct().forEach(System.out::println);
+
+		List<String> dishNames = getMenu().stream().map(Dish::getName).collect(Collectors.toList());
+		dishNames.stream().forEach(System.out::println);
+
+		getMenu().stream().map(dish -> dish.getName().length()).forEach(System.out::println);
+
+		List<String> uniqueCharacters = getWords().stream().map(s -> s.split("")).flatMap(Arrays::stream).distinct()
+				.collect(Collectors.toList());
+		uniqueCharacters.stream().forEach(System.out::println);
+
 	}
 
 	public static List<Dish> getMenu() {
@@ -22,6 +40,11 @@ public class MainMenu {
 				new Dish("rice", true, 350, Dish.Type.OTHER), new Dish("season fruit", true, 120, Dish.Type.OTHER),
 				new Dish("pizza", true, 550, Dish.Type.OTHER), new Dish("prawns", false, 300, Dish.Type.FISH),
 				new Dish("salmon", false, 450, Dish.Type.FISH));
+	}
+
+	public static List<String> getWords() {
+		String[] arrayOfWords = { "Goodbye", "World", "Hello", "World", "Again" };
+		return Arrays.asList(arrayOfWords);
 	}
 
 }
